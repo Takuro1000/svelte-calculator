278432b4853bd0a9e49e4fe4011cbe32edee76fb
Takuro1000 <taku.0116.19@gmail.com>
2024-02-28 14:12:19 +0900

    Svelteの計算機機能を追加

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
-
+  //alert("welcome to svelte calculator!");
 </script>
 
-<main>
-  <h1>Hello World</h1>
-  <p>Welcome to SvelteKit</p>
+<main class="space-y-8">
+  <h1>SVELTE CALCULATOR</h1>
+  <div>
+    <input type="text" placeholder="input number 1">
+    <input type="text" placeholder="input number 2">
+  </div>
+  <div>
+    <button>ADD</button>
+    <button>SUBTRACT</button>
+    <button>MULTIPLY</button>
+    <button>DIVIDE</button>
+  </div>
+  <div>
+    <h2>Result: 0</h2>
+  </div>
 </main>
